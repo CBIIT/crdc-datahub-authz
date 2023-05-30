c790abe5dc10ef464242b0a9cadb791afd693bef
Wesley Lau <wesley.lau@nih.org>
2023-05-30 09:39:13 -0400

    init repo

---
+++ bento-event-logging/const/access-constant.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+    // arm status
+    PENDING: 'Pending',
+    APPROVED: 'Approved',
+    REJECTED: 'Rejected',
+    REVOKED: 'Revoked',
+});
---
+++ bin/user-constant.js
@@ -0,0 +1,14 @@
+module.exports = Object.freeze({
+    // user status
+    NONE: '',
+    INACTIVE: 'Inactive',
+    ACTIVE: 'Active',
+    DISABLED: 'Disabled',
+    DELETED: 'Deleted',
+    // user role
+    ADMIN: 'Admin',
+    MEMBER: 'Member',
+    NON_MEMBER: 'Non-Member',
+    // other user constants
+    NOT_APPLICABLE: "Not Applicable"
+});
