0344eb4e0125833d72305f340f3b7827871eb197
Wesley Lau <wesley.lau@nih.org>
2023-06-12 09:33:19 -0400

    Updated name of the file

--- routers/graphql-router.js
+++ routers/graphql-router.js
@@ -2,8 +2,8 @@ const {buildSchema} = require('graphql');
 const {createHandler} = require("graphql-http/lib/use/express");
 const config = require("../config");
 
-const schema = buildSchema(require("fs").readFileSync("resources/graphql/placeholder.graphql", "utf8"));
-// const schema = buildSchema(require("fs").readFileSync("resources/graphql/crdc-datahub.graphql", "utf8"));
+const schema = buildSchema(require("fs").readFileSync("resources/graphql/user_test.graphql", "utf8"));
+// const schema = buildSchema(require("fs").readFileSync("resources/graphql/placeholder.graphql", "utf8"));
 
 
 // crdc-datahub
--- routers/status-endpoints-router.js
+++ routers/status-endpoints-router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/ping", (req, res, next) => {
     res.send('pong');
 });
-router.get("/somethin", (req, res, next) => {
+router.get("/user", (req, res, next) => {
     res.json({
         version: config.version,
         date: config.date,
