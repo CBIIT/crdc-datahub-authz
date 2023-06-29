de3094de444aabbfe84147df3ed102bc4870a4d3
Austin Mueller <austinsmueller@gmail.com>
2023-06-29 15:40:56 -0400

    CRDCDH-73
    update submodule and resolve merge issues

--- routers/graphql-router.js
+++ routers/graphql-router.js
@@ -5,18 +5,16 @@ const {MongoQueries} = require("../crdc-datahub-database-drivers/mongo-queries")
 const {MongoDBCollection} = require("../crdc-datahub-database-drivers/mongodb-collection");
 const {DATABASE_NAME, USER_COLLECTION} = require("../crdc-datahub-database-drivers/database-constants");
 const {DatabaseConnector} = require("../crdc-datahub-database-drivers/database-connector");
-const {User} = require("./user")
+const {User} = require("../crdc-datahub-database-drivers/services/user")
 
 const schema = buildSchema(require("fs").readFileSync("resources/graphql/authorization.graphql", "utf8"));
-const dbService = new MongoQueries(config.mongo_db_connection_string, DATABASE_NAME);
 const dbConnector = new DatabaseConnector(config.mongo_db_connection_string);
 
 let root;
 dbConnector.connect().then(() => {
-    const userColletion = new MongoDBCollection(dbConnector.client, DATABASE_NAME, USER_COLLECTION);
-    const dataInterface = new User(userColletion, dbService);
+    const userCollection = new MongoDBCollection(dbConnector.client, DATABASE_NAME, USER_COLLECTION);
+    const dataInterface = new User(userCollection);
     root = {
-        version: () => {return config.version},
         getMyUser : dataInterface.getMyUser.bind(dataInterface)
     };
 });
