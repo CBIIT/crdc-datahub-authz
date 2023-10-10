26575a531f53b1e59d82c79470e7adb055b92ac0
Wesley Lau <wesley.lau@nih.org>
2023-10-10 12:18:16 -0400

    Use session secret for token secret

--- config.js
+++ config.js
@@ -16,7 +16,6 @@ let config = {
     session_timeout: parseInt(process.env.SESSION_TIMEOUT_SECONDS) * 1000 || 30 * 60 * 1000,
 
     // Token
-    token_secret: process.env.TOKEN_SECRET,
     tokenTimeout: parseInt(process.env.TOKEN_TIMEOUT)*1000 || 60*24*60*60*1000
 };
 config.mongo_db_connection_string = `mongodb://${config.mongo_db_user}:${config.mongo_db_password}@${config.mongo_db_host}:${process.env.MONGO_DB_PORT}`;
